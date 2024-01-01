ff250d6b6ce9a392ccd4a4411765e34f980f087d
oipik <wazuup.lw@mail.ru>
2024-01-01 16:51:28 +0300

    Finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { evaluate } from "mathjs";
 
 import signal from "../images/signal.png";
 import wifi from "../images/wifi.png";
@@ -14,9 +15,12 @@ import dark from "../images/dark.png";
 import './app.scss';
 
 import Button from "./Button";
+import { type } from "@testing-library/user-event/dist/type";
 
 function App() {
     const [theme, setTheme] = useState("light");
+    const [value, setValue] = useState("");
+    const [rec, setRec] = useState("");
 
     const getTime = () => {
         const date = new Date();
@@ -26,7 +30,6 @@ function App() {
     }
 
     const changeTheme = () => {
-        console.log("hello")
         if (theme === "light") {
             setTheme("dark");
         } else {
@@ -34,9 +37,30 @@ function App() {
         }
     }
 
+    const reset = () => {
+        setValue("");
+        setRec("");
+    }
+
+    const handleInputValue = (val) => {
+        if ((val >= 0 || val <= 9) && value != "") {
+            setValue("");
+            setRec("")
+            setRec(rec => rec + val)
+        } else if ( (val === "%" || val === "/" || val === "*" || val === "-" || val === "+") && value != "") {
+            setValue("");
+            setRec(value + val);
+        } else if (val !== "=") {
+            setRec(rec => rec + val)
+        } else if (val === "=") {
+            setValue(evaluate(rec));
+            setRec(rec => rec + val)
+        }
+    }
+
     return (
         <div className="wrapper">
-            <div className="calc">
+            <div className={`calc ${theme}`}>
                 <div className="calc__window">
                     <div className="calc__sidebar">
                         <p className="calc__bar">{getTime()}</p>
@@ -47,30 +71,30 @@ function App() {
                         </div>
                     </div>
                     <div className="calc__values">
-                        <p className="first">Первое</p>
-                        <p className="second">Второе</p>
+                        <p className="first">{rec}</p>
+                        <p className="second">{value}</p>
                     </div>
                 </div>
                 <div className="calc__buttons">
                     <Button changeTheme={changeTheme}><img src={theme === "light" ? light : dark} alt="theme"/></Button>
-                    <Button>%</Button>
-                    <Button>/</Button>
-                    <Button>*</Button>
-                    <Button>7</Button>
-                    <Button>8</Button>
-                    <Button>9</Button>
-                    <Button>-</Button>
-                    <Button>4</Button>
-                    <Button>5</Button>
-                    <Button>6</Button>
-                    <Button>+</Button>
-                    <Button>1</Button>
-                    <Button>2</Button>
-                    <Button>3</Button>
-                    <Button>AC</Button>
-                    <Button>0</Button>
-                    <Button>.</Button>
-                    <Button>=</Button>
+                    <Button handleInputValue={handleInputValue}>%</Button>
+                    <Button handleInputValue={handleInputValue}>/</Button>
+                    <Button handleInputValue={handleInputValue}>*</Button>
+                    <Button handleInputValue={handleInputValue}>7</Button>
+                    <Button handleInputValue={handleInputValue}>8</Button>
+                    <Button handleInputValue={handleInputValue}>9</Button>
+                    <Button handleInputValue={handleInputValue}>-</Button>
+                    <Button handleInputValue={handleInputValue}>4</Button>
+                    <Button handleInputValue={handleInputValue}>5</Button>
+                    <Button handleInputValue={handleInputValue}>6</Button>
+                    <Button handleInputValue={handleInputValue}>+</Button>
+                    <Button handleInputValue={handleInputValue}>1</Button>
+                    <Button handleInputValue={handleInputValue}>2</Button>
+                    <Button handleInputValue={handleInputValue}>3</Button>
+                    <Button reset={reset}>AC</Button>
+                    <Button handleInputValue={handleInputValue}>0</Button>
+                    <Button handleInputValue={handleInputValue}>.</Button>
+                    <Button handleInputValue={handleInputValue}>=</Button>
                 </div>
                 <hr className="calc__indicator"/>
             </div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import "./button.scss";
 
 const Button = (props) => {
 
-    console.log(props)
+    const theme = typeof props.children === "object" ? "theme" : "";
 
     const getClass = () => {
         switch (props.children) {
@@ -17,15 +17,51 @@ const Button = (props) => {
             case "AC":
                 return "ac"
             default:
-                return null;
+                return "";
         }
     }
 
     return (
-        <button className={`btn ${getClass()}`} onClick={() => props.changeTheme}>
+        <button
+            className={`btn ${getClass()}${theme}`}
+            onClick={
+                props.changeTheme ? () => props.changeTheme() : 
+                props.handleInputValue ? () => props.handleInputValue(props.children) : () => props.reset()}>
             {props.children}
         </button>
     )
 }
 
+
 export default Button;
+
+
+// const Button = (props) => {
+
+    //     const theme = typeof props.children === "object" ? "theme" : "";
+    
+    //     const getClass = () => {
+    //         switch (props.children) {
+    //             case "%":
+    //             case "/":
+    //             case "*":
+    //             case "+":
+    //             case "-":
+    //                 return "action";
+    //             case "=":
+    //                 return "equal";
+    //             case "AC":
+    //                 return "ac"
+    //             default:
+    //                 return "";
+    //         }
+    //     }
+    
+    //     return (
+    //         <button
+    //             className={`btn ${getClass()}${theme}`}
+    //             onClick={props.changeTheme ? () => props.changeTheme() : props.handleInputValue}>
+    //             {props.children}
+    //         </button>
+    //     )
+    // }
